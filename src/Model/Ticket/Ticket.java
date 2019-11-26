22af75cbc8c29b8025c60acd7d10a87f97cdcacc
anna <ania.brytanczyk@gmail.com>
2019-11-26 05:18:10 +0100

    Probably working Coordinator view + Tickets added getter to sth needed

--- src/Model/Ticket/Ticket.java
+++ src/Model/Ticket/Ticket.java
@@ -57,6 +57,7 @@ public class Ticket {
 
     public TicketStatus status(){return  this.status;}
 
+
     public static Optional<Ticket> create(int coordinatorID, int userID, String title, String description) {
         String insertSql = String.format("INSERT INTO %s (COORDINATOR_ID, USER_ID, TITLE, DESCRIPTION, STATUS) VALUES (%d, %d, '%s', '%s', 'WAITING');", TABLE_NAME,
                 coordinatorID, userID, title, description);
